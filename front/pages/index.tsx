76880773633d54997e42613d027a257ed5c8078c
c.ishii <chiichiitei_0711@yahoo.co.jp>
2022-04-09 11:23:00 +0900

    #10

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,19 +1,20 @@
 import type { NextPage } from "next";
 import { AxiosError, AxiosResponse } from "axios";
-import { ChangeEvent, useState } from "react";
+import { useState } from "react";
 import { RequiredMark } from "../components/RequiredMark";
 import { axiosApi } from "../lib/axios";
 import { useRouter } from "next/router";
 import { useUserState } from "../atoms/userAtom";
+import { useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
 
-// POSSTデータの型
+// POSTデータの型
 type LoginForm = {
   email: string;
   password: string;
 };
 
 // バリデーションメッセージの型
-// 各プロパティのundefined許容
 type Validation = {
   email?: string;
   password?: string;
@@ -21,25 +22,21 @@ type Validation = {
 };
 
 const Home: NextPage = () => {
+  // ルーター定義
   const router = useRouter();
-  // POSTデータのstate
-  const [loginForm, setLoginForm] = useState<LoginForm>({
-    email: "",
-    password: "",
-  });
+  // state定義
+  const [validation, setValidation] = useState<Validation>({});
+  // recoil stateの呼び出し
   const { setUser } = useUserState();
 
-  // バリデーションメッセージのstate
-  // validationのプロパティをundefinedに許容したので初期値を空オブジェクトに
-  const [validation, setValidation] = useState<Validation>({});
-
-  // POSTデータの更新
-  const updateLoginForm = (e: ChangeEvent<HTMLInputElement>) => {
-    setLoginForm({ ...loginForm, [e.target.name]: e.target.value });
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>();
 
   // ログイン
-  const login = () => {
+  const login = (data: LoginForm) => {
     // バリデーションメッセージの初期化
     setValidation({});
 
@@ -49,7 +46,7 @@ const Home: NextPage = () => {
       .then((res) => {
         // ログイン処理
         axiosApi
-          .post("/login", loginForm)
+          .post("/login", data)
           .then((response: AxiosResponse) => {
             setUser(response.data.data);
             router.push("/memos");
@@ -60,7 +57,6 @@ const Home: NextPage = () => {
             if (err.response?.status === 422) {
               const errors = err.response?.data.errors;
               // state更新用のオブジェクトを別で定義
-              // validationMessagesを型アサーションasでValidationの型のオブジェクトとして扱えるようにした（state更新の内容をvalidationMessagesそのものとするため）
               const validationMessages: { [index: string]: string } =
                 {} as Validation;
               Object.keys(errors).map((key: string) => {
@@ -70,7 +66,7 @@ const Home: NextPage = () => {
               setValidation(validationMessages);
             }
             if (err.response?.status === 500) {
-              alert("システムエラーです");
+              alert("システムエラーです！！");
             }
           });
       });
@@ -87,9 +83,20 @@ const Home: NextPage = () => {
           </div>
           <input
             className="p-2 border rounded-md w-full outline-none"
-            name="email"
-            value={loginForm.email}
-            onChange={updateLoginForm}
+            {...register("email", {
+              required: "必須入力です。",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "有効なメールアドレスを入力してください。",
+              },
+            })}
+          />
+          <ErrorMessage
+            errors={errors}
+            name={"email"}
+            render={({ message }) => (
+              <p className="py-3 text-red-500">{message}</p>
+            )}
           />
           {validation.email && (
             <p className="py-3 text-red-500">{validation.email}</p>
@@ -105,10 +112,21 @@ const Home: NextPage = () => {
           </small>
           <input
             className="p-2 border rounded-md w-full outline-none"
-            name="password"
             type="password"
-            value={loginForm.password}
-            onChange={updateLoginForm}
+            {...register("password", {
+              required: "必須入力です。",
+              pattern: {
+                value: /^([a-zA-Z0-9]{8,})$/,
+                message: "8文字以上の半角英数字で入力してください",
+              },
+            })}
+          />
+          <ErrorMessage
+            errors={errors}
+            name={"password"}
+            render={({ message }) => (
+              <p className="py-3 text-red-500">{message}</p>
+            )}
           />
           {validation.password && (
             <p className="py-3 text-red-500">{validation.password}</p>
@@ -120,7 +138,7 @@ const Home: NextPage = () => {
           )}
           <button
             className="bg-gray-700 text-gray-50 py-3 sm:px-20 px-10 rounded-xl cursor-pointer drop-shadow-md hover:bg-gray-600"
-            onClick={login}
+            onClick={handleSubmit(login)}
           >
             ログイン
           </button>
